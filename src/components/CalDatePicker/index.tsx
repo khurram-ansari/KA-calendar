dbed0b349fb1309f482eceb27a56975a99661910
khurram-ansari <khury.slm@gmail.com>
2024-08-31 15:23:43 +0500

    add constants dir

--- src/components/CalDatePicker/index.tsx
+++ src/components/CalDatePicker/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { DatePickerWithPresets } from '../DatePickerWithPresets';
+import { BASE_DATE_LOCALE } from '@/constants';
 
 export default function CalDatePicker() {
   const navigate = useNavigate();
 
   const handleDateChange = (date?: Date) => {
-    const dateString = date?.toLocaleDateString('en-US');
+    const dateString = date?.toLocaleDateString(BASE_DATE_LOCALE);
 
     if (dateString) {
       navigate({
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const BASE_DATE_LOCALE = 'en-US';
