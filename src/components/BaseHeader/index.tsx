e1a0c22cc84d96b7356d53ff762a9bfe900af8a4
khurram-ansari <khury.slm@gmail.com>
2024-08-31 13:27:04 +0500

    add user dropdown item click handler in header component

--- src/components/BaseHeader/index.tsx
+++ src/components/BaseHeader/index.tsx
@@ -1,21 +1,39 @@
 import { useAuth } from '@/contexts/useAuth';
-import { Button } from '../ui/button';
 import { useNavigate } from 'react-router-dom';
+import UserDropdown from '../UserDropdown';
 
 const BaseHeader = () => {
   const { session, logout } = useAuth();
   const navigate = useNavigate();
+
+  const { user } = session ?? {};
 
   const handleLogout = async () => {
     const res = await logout();
 
     if (res) navigate('/login');
   };
+
+  const dropdownActions: Record<string, () => void> = {
+    LOGOUT: handleLogout,
+  };
+
+  const handleDropdownAction = (key: string) => {
+    if (!key || !dropdownActions[key]) return;
+
+    dropdownActions[key]();
+  };
   return (
-    <header>
-      <div className="container flex items-center justify-between">
-        <div>{JSON.stringify(session?.user?.email)}</div>
-        <Button onClick={handleLogout}>Logout</Button>
+    <header className="h-16 border-b p-2">
+      <div className="container flex h-full items-center justify-between">
+        {/* USER AVATAR AND DROPDOWN */}
+        {user && (
+          <UserDropdown
+            onItemClicked={handleDropdownAction}
+            userImg={user?.user_metadata?.avatar_url}
+          />
+        )}
+        {/* <Button onClick={handleLogout}>Logout</Button> */}
       </div>
     </header>
   );
